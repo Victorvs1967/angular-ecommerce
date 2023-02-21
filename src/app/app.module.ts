25f21a872085bb7fd5b004a25cc5e29cee87e6a1
Victorvs1967 <victorsmirnov@me.com>
2023-02-21 16:01:33 +0200

    create base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    HeaderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
